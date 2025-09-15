ae355b6b766276571d08f97782cc6ae736461411
gabrielfelps <ugabrielfelipe@icloud.com>
2025-09-15 11:55:49 -0300

    refactor: Separa a lógica para criar e desenhar figuras

--- src/main/java/br/com/mariojp/figureeditor/DrawingPanel.java
+++ src/main/java/br/com/mariojp/figureeditor/DrawingPanel.java
@@ -6,7 +6,6 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +13,7 @@ class DrawingPanel extends JPanel {
 
     private static final long serialVersionUID = 1L;
     private static final int DEFAULT_SIZE = 60;
-    private final List<Shape> shapes = new ArrayList<>();
+    private final List<Figure> figures = new ArrayList<>();
     private Point startDrag = null;
 
     DrawingPanel() {
@@ -27,9 +26,8 @@ class DrawingPanel extends JPanel {
             @Override public void mouseClicked(MouseEvent e) {
                 if (e.getClickCount() == 1 && startDrag == null) {
                     int size = Math.max(Math.min(DEFAULT_SIZE, DEFAULT_SIZE), 10);
-                    Shape s =  new Ellipse2D.Double(e.getPoint().x, e.getPoint().y, size, size);
-                    //return new Rectangle2D.Double(e.getPoint().x, e.getPoint().y, Math.max(DEFAULT_SIZE, 10), Math.max(DEFAULT_SIZE, 10));
-                    shapes.add(s);
+                    Figure f = FigureFactory.createFigure(FigureFactory.FigureType.ELLIPSE, e.getPoint());
+                    figures.add(f);
                     repaint();
                 }
             }
@@ -40,7 +38,7 @@ class DrawingPanel extends JPanel {
     }
 
     void clear() {
-        shapes.clear();
+        figures.clear();
         repaint();
     }
 
@@ -49,12 +47,8 @@ class DrawingPanel extends JPanel {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        for (Shape s : shapes) {
-            g2.setColor(new Color(30,144,255));
-            g2.fill(s);
-            g2.setColor(new Color(0,0,0,70));
-            g2.setStroke(new BasicStroke(1.2f));
-            g2.draw(s);
+        for (Figure f : figures) {
+            f.draw(g2);
         }
 
         g2.dispose();
